49e47df36981dbc20c6e610a0b5637945dc551dd
sergio <sergio@sergiotorre>
2012-06-21 10:10:01 +0200

    project.sh has been fixed.

--- 03_application/django/project.sh
+++ 03_application/django/project.sh
@@ -27,7 +27,7 @@ function createDjangoProject {
 	projectsDirectory=$2 # Second argument is projects directory.
 	cd $projectsDirectory
 	django-admin.py startproject $projectName
-	stringPath='    os.path.join(os.path.dirname(__file__) , "documents").replace("\\\\","/").replace("mysite/mysite","mysite"),'
+	stringPath='    os.path.join(os.path.dirname(__file__) , "documents").replace("\\\\","/").replace("'$projectName'/'$projectName'","'$projectName'"),'
 	sed -i -e '1a\import os.path' -e '/^TEMPLATE_DIRS = ($/a\'"$stringPath" $projectName"/"$projectName"/settings.py"
 }
 function removeDjangoProject {
